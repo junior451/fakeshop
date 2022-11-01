126ae94a8d94c63323b98a6c19612a44e4e7c8f7
junior451 <kwabenaedwards@gmail.com>
2022-11-01 21:15:40 +0000

    add css styles and validate for products model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create!(title: 'Build Chatbot Interactions',
+  description:
+    %{<p>
+      <em>Responsive, Intuitive Interfaces with Ruby</em>
+      The next step in the evolution of user interfaces is here. 
+      Chatbots let your users interact with your service in their
+      own natural language. Use free and open source tools along 
+      with Ruby to build creative, useful, and unexpected interactions 
+      for users. Take advantage of the Lita framework’s step-by-step 
+      implementation strategy to simplify bot development and testing.
+      From novices to experts, chatbots are an area in which everyone 
+      can participate. Exercise your creativity by creating chatbot 
+      skills for communicating, information, and fun.
+      </p>},
+  image_url: 'dpchat.jpg',
+  price: 20.00)
+# . . .
+
+Product.create!(title: 'Programming Crystal',
+  description:
+    %{<p>
+      <em>Create High-Performance, Safe, Concurrent Apps</em>
+      Crystal is for Ruby programmers who want more performance or for 
+      developers who enjoy working in a high-level scripting environment. Crystal 
+      combines native execution speed and concurrency with Ruby-like syntax, so 
+      you will feel right at home. This book, the first available on Crystal, 
+      shows you how to write applications that have the beauty and elegance of a 
+      modern language, combined with the power of types and modern concurrency 
+      tooling. Now you can write beautiful code that runs faster, scales better, 
+      and is a breeze to deploy.
+      </p>},
+  image_url: 'crystal.jpg',
+  price: 40.00)
